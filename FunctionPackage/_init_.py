05cd9b024914c52853c31a9317db0df3a89f09b0
Motselisi <48673380+Motselisi@users.noreply.github.com>
2019-03-19 13:37:40 +0200

    Update _init_.py

--- FunctionPackage/_init_.py
+++ FunctionPackage/_init_.py
@@ -1,3 +1,3 @@
 global sum_array, fibonacci, factorial, reverse, bubble_sort, merge_sort, quick_sort
-    from FunctionPackage.recursion import sum_array, fibonacci, factorial, reverse
-    from FunctionPackage.sorting import bubble_sort, merge_sort, quick_sort
+from FunctionPackage.recursion import sum_array, fibonacci, factorial, reverse
+from FunctionPackage.sorting import bubble_sort, merge_sort, quick_sort
