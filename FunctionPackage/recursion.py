171ab67a31fcc29ce3f87011ee0dbc83bde8b6b9
Motselisi <48673380+Motselisi@users.noreply.github.com>
2019-03-19 13:41:49 +0200

    Update recursion.py

--- FunctionPackage/recursion.py
+++ FunctionPackage/recursion.py
@@ -1,15 +1,12 @@
 def sum_array(array):
     return(sum(array))
 
-
-
 def fibonacci(n):
     if n <= 1:
         return n
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
-
-
+    
 def factorial(n):
     if n <1:
         return 1
@@ -17,7 +14,6 @@ def factorial(n):
         returnNumber = n * factorial( n - 1 )  
     print(str(n) + '! = ' + str(returnNumber))
     return returnNumber
-
 
 def reverse(word):
     if word == "":
